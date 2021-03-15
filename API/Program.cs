53214c4a1644258a4a58fd63d9aabf9e83591d5d
PhongHua <phonghua1989@hotmail.com>
2021-03-15 21:01:53 +1100

    Init git

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,42 @@
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //Create host variable and store this command inside host
+            var host = CreateHostBuilder(args).Build();
+            // using keyword means once we finish with this method 'Main'
+            // the scope is gonna be disposed by the framework
+            // We're going to create a scope that's going to host any services
+            // that we create inside this particular method
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try 
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during migration");
+            }
+            // Dont forget to run application
+            await host.RunAsync();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
